cc00937d2bf7bc2871d95759b809b4e6d224c187
Jason Korol <ejkorol@gmail.com>
2024-12-16 20:58:46 -0500

    chore: adding eslint

---
+++ packages/eslint-config/base.js
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    ignores: ["**/build/**/*", "**/dist/**/*", "**/*.d.ts"],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
