a450ef5e04e58b623b9767e07cdd113baf74a232
Jason Korol <ejkorol@gmail.com>
2024-12-19 11:54:11 -0500

    adds test suite for APCards

---
+++ apps/api/test/engine/components/ActionPointCard.test.ts
@@ -0,0 +1,29 @@
+import {
+  beforeEach,
+  afterEach,
+  describe,
+  it,
+  expect,
+} from '@jest/globals'
+import { ActionPointCard } from '../../../src/engine/components/ActionPointCard'
+import type { ActionPointCard as ActionPointCardType } from '../../../src/types'
+import { mockActionCardData } from '../../fixtures/card-fixture'
+
+describe('ActionPointCard', () => {
+  let apCard: ActionPointCardType
+
+  beforeEach(done => {
+    apCard = new ActionPointCard(mockActionCardData.name)
+    done()
+  })
+
+  afterEach(done => {
+    done()
+  })
+
+  it('initializes a card correctly', () => {
+    expect(apCard.name).toBe(mockActionCardData.name)
+    expect(apCard.isFaceUp).toBeFalsy()
+    expect(apCard.isRested).toBeFalsy()
+  })
+})
