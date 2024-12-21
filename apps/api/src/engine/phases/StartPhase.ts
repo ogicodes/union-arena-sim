fa44b1dda0f1016fb3e127b70e28a2bc81c7c7ac
Jason Korol <ejkorol@gmail.com>
2024-12-20 22:43:36 -0500

    creates initial start phase logic

--- apps/api/src/engine/phases/StartPhase.ts
+++ apps/api/src/engine/phases/StartPhase.ts
@@ -10,10 +10,84 @@ class StartPhase extends Phase {
     this._gameState = gameState
   }
 
+  /**
+   * protected _execute
+   *
+   * The main entry point that gets executed
+   *
+   * @returns void
+   * */
   protected _execute(): void {
-    console.log(
-      `Executing: ${this._name} @ ${this._gameState.currentPhaseIdx}`,
+    this.unrestCards()
+    this.flipActionPoints()
+  }
+
+  /**
+   * private unrestCards
+   *
+   * Takes the current activePlayer and unrests
+   * any cards that are currently resting
+   *
+   * @returns void
+   * */
+  private unrestCards(): void {
+    const { activePlayer } = this._gameState
+    const { actionPointsLine, frontLine, energyLine } =
+      this._gameState.getBoard(activePlayer.id)
+    actionPointsLine.forEach(card => {
+      if (card.data.state.isRested) {
+        card.rest()
+      }
+    })
+    frontLine.forEach(card => {
+      if (card.data.state.isRested) {
+        card.rest()
+      }
+    })
+    energyLine.forEach(card => {
+      if (card.data.state.isRested) {
+        card.rest()
+      }
+    })
+  }
+
+  /**
+   * private flipActionPoints
+   *
+   * Calculates how many action point cards
+   * need to flip for each player
+   *
+   * @returns void
+   * */
+  private flipActionPoints(): void {
+    const { turnCount, activePlayer } = this._gameState
+    const { actionPointsLine } = this._gameState.getBoard(
+      activePlayer.id,
     )
+
+    if (turnCount > 3) return
+
+    let flipsToMake = 0
+
+    if (activePlayer.id === this._gameState.players[0].id) {
+      if (turnCount === 1) {
+        flipsToMake = 1
+      } else if (turnCount === 2) {
+        flipsToMake = 2
+      } else if (turnCount >= 3) {
+        flipsToMake = 3
+      }
+    } else if (activePlayer.id === this._gameState.players[1].id) {
+      if (turnCount === 1 || turnCount === 2) {
+        flipsToMake = 2
+      } else if (turnCount >= 3) {
+        flipsToMake = 3
+      }
+    }
+
+    for (let i = 0; i < flipsToMake; i++) {
+      actionPointsLine[i].flip()
+    }
   }
 }
 
